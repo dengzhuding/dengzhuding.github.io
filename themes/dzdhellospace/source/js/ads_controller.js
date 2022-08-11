7badccb36c34d64d18d666aeed7b81b2a4c5a660
dzd_global <dengzhuding@123.com>
2022-08-11 17:51:56 +0800

    fix: ads add units

--- themes/dzdhellospace/source/js/ads_controller.js
+++ themes/dzdhellospace/source/js/ads_controller.js
@@ -3,7 +3,7 @@
  */
 (function () {
   const adsOpenStatus = localStorage.getItem('googole-ads-open');
-  if (adsOpenStatus !== null && adsOpenStatus !== 'true') {
+  if (adsOpenStatus !== 'true') {
     return;
   }
   const s = document.createElement('script');
@@ -11,4 +11,18 @@
   s.crossOrigin = 'anonymous';
   s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5071251895443211';
   document.querySelector('head').appendChild(s);
+
+  // test-unit-ads 
+  let ins_str = `<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5071251895443211"
+     data-ad-slot="7161676001"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`;
+  const adsDiv = document.querySelector('.ads-div');
+  if (!adsDiv) {
+    return;
+  }
+  adsDiv.innerHTML = ins_str;
+  (adsbygoogle = window.adsbygoogle || []).push({});
 })()
