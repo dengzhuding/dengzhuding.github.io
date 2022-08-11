e7ecba3c0d8492fcce37053f75025fd93ce686ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-08-11 10:09:22 +0000

    Deploy dengzhuding/dengzhuding.github.io to github.com/dengzhuding/dengzhuding.github.io.git:gh-pages

--- js/ads_controller.js
+++ js/ads_controller.js
@@ -6,11 +6,22 @@
   if (adsOpenStatus !== 'true') {
     return;
   }
+
+  const head = document.querySelector('head');
+
+  // 未填充的广告位置设置隐藏
+  const ads_css = document.createElement('style');
+  ads_css.innerHTML = `
+    ins.adsbygoogle[data-ad-status="unfilled"] {
+      display: none !important;
+    }`;
+  head.appendChild(ads_css);
+  
   const s = document.createElement('script');
   s.async = true;
   s.crossOrigin = 'anonymous';
   s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5071251895443211';
-  document.querySelector('head').appendChild(s);
+  head.appendChild(s);
 
   // test-unit-ads 
   let ins_str = `<ins class="adsbygoogle"
